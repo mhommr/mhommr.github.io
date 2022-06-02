3cc77e41c16bed8c4b957dec2e25a09825ad6733
mhommr <mhommrich@gmail.com>
2022-06-02 11:13:47 -0400

    Draft1
    Fixed Share and Act, added some JavaScript and more images.

---
+++ BellabeatJS.js
@@ -0,0 +1,61 @@
+console.log("Hello world!")
+
+function myfunction() {
+    document.getElementById("Panel").style.display = "flex";
+}
+
+function myfunction2() {
+    document.getElementById("Panel2").style.display = "block";
+}
+
+function myfunction3() {
+    document.getElementById("Panel3").style.display = "block"
+}
+
+function myfunction4() {
+    document.getElementById("Panel4").style.display = "block"
+}
+
+function myfunction5() {
+    document.getElementById("Panel5").style.display = "block"
+}
+
+function myfunction6() {
+    document.getElementById("Panel6").style.display = "block"
+}
+
+function myfunction7() {
+    document.getElementById("Panel7").style.display = "block"
+}
+
+function myfunctions() {
+    document.getElementsByClassName("Panels").style.display = "block"
+}
+
+function myfunc() {
+    document.getElementById("Panel").style.display = "none"
+}
+
+function myfunc2() {
+    document.getElementById("Panel2").style.display = "none"
+}
+
+function myfunc3() {
+    document.getElementById("Panel3").style.display = "none"
+}
+
+function myfunc4() {
+    document.getElementById("Panel4").style.display = "none"
+}
+
+function myfunc5() {
+    document.getElementById("Panel5").style.display = "none"
+}
+
+function myfunc6() {
+    document.getElementById("Panel6").style.display = "none"
+}
+
+function myfunc7() {
+    document.getElementById("Panel7").style.display = "none"
+}
