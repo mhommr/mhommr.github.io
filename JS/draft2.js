974ffa21c1ea0522c243b6676b9e3be622ee9156
mhommr <mhommrich@gmail.com>
2023-01-09 14:53:54 -0500

    Add JavaScript Breakpoints

--- JS/draft2.js
+++ JS/draft2.js
@@ -6,45 +6,79 @@ function expandNav() {
     }
 }
 
+const mql = window.matchMedia("(max-width:640px)");
+
+// mql.onload = helpMe(mql);
+// mql.onchange = helpMe(mql);
+
 function getBig(image) {
-    if (document.getElementById('text').style.display == 'inline-block') {
-        document.getElementById('text').style.display = 'none';
-    } else {
-        document.getElementById('text').style.display = 'inline-block';
-    }
-    
-    if (document.getElementById('active').backgroundImage == undefined) {
-        document.getElementById('active').id = "activeBig";
-        document.getElementById('activeBig').style.backgroundImage = "url("+image.src+")";
-        document.getElementById('alt').innerHTML = image.alt;
-        console.log(document.getElementById('activeBig').backgroundImage)
-    }
-    
-    // if (document.getElementById('placeholder').img == undefined) {
-    //     document.getElementById('active').id = "activeBig";
-    //     console.log(document.getElementById(''))
-    //     document.getElementById('activeBig').style.backgroundImage = "url("+image.src+")"
-    //     document.getElementById('alt').innerHTML = image.alt;
-    // }
+    document.getElementById('text').style.display = 'none';
+
+    const active = document.getElementById('active');
+
+    active.style.backgroundImage = "url("+image.src+")";
+    active.className = 'activeClassBig'
+    document.getElementById('alt').innerHTML = image.alt;
+
+    document.getElementById('textSmall').style.display = 'none';
 
 
-    /*document.getElementById("active").style.backgroundImage = "url("+image.src+")";
-    document.getElementById("active").style.backgroundColor = "#000000";
-    document.getElementById("alt").innerHTML = image.alt;*/
+    const activeSmall = document.getElementById('activeSmall');
+    activeSmall.style.backgroundImage = "url("+image.src+")";
+    activeSmall.className = 'activeClassBig';
+    document.getElementById('altSmall') = image.alt;
 }
 
 function getSmall() {
-    console.log(document.getElementById('activeBig').backgroundImage)
-    if (document.getElementById('text').style.display == 'none') {
-        document.getElementById('text').style.display = 'inline-block'
-    }
-    document.getElementById('activeBig').id='active';
-    document.getElementById('active').style.backgroundImage = "url(' ')";
-    document.getElementById('active').style.backgroundColor = "#4e6766";
-    document.getElementById('alt').innerHTML = "";
-    
+        if (document.getElementById('text').style.display == 'none') {
+            document.getElementById('text').style.display = 'inline-block'
+        }
+        const active = document.getElementById('active');
+        active.style.backgroundImage = "url(' ')";
+        active.style.backgroundColor = "#4e6766";
+        active.className = 'activeClass'
+        document.getElementById('alt').innerHTML = "";
 
-    // document.getElementById("active").style.backgroundImage = "url(' ')";
-    // document.getElementById("active").style.backgroundColor = "#4e6766";
-    // document.getElementById("alt").innerHTML = ""
+        if (document.getElementById('textSmall').style.display == 'none') {
+            document.getElementById('textSmall').style.display = 'inline-block'
+        }
+        const activeSmall = document.getElementById('activeSmall');
+        activeSmall.style.backgroundImage = "url(' ')";
+        activeSmall.style.backgroundColor = '#4e6766';
+        activeSmall.className = 'activeClass';
+        document.getElementById('altSmall').innerHTML = '';
 }
+
+window.onload = function () {
+
+    const text = document.getElementById('text');
+    
+    function mediaQuery (ql) {
+        if (ql.matches) {
+            console.log("geez");
+            document.getElementById('bigScreen').style.display = "none";
+            document.getElementById('smallScreen').style.display = 'block';
+          } else {
+            console.log("help");
+            document.getElementById('smallScreen').style.display = 'none';
+            document.getElementById('bigScreen').style.display = 'block';
+          }
+    }
+
+    mql.addEventListener('load', mediaQuery(mql))
+    mql.addEventListener('change', mediaQuery)
+}
+
+
+// I think this is a functioning breakpoint function... just in case we'll save it as a comment
+// function myFunction(x) {
+//     if (x.matches) { // If media query matches
+//       document.getElementById('text').innerHTML = "It worked"
+//     } else {
+//       document.body.style.backgroundColor = "pink";
+//     }
+//   }
+
+// var x = window.matchMedia("(max-width: 640px)")
+// myFunction(x) // Call listener function at run time
+// x.addEventListener(myFunction) // Attach listener function on state changes
